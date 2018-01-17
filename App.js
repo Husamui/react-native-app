b1ba4a13cfcc0f5d2cf55fc781c97687c4314e72
Husamui <husamalrubaye@gmail.com>
2018-01-16 22:13:14 -0800

    Added basci routing

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// Navigations
+import AppNavigation from './src/navigater';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppNavigation />
+    );
+  }
+}
+
---
+++ src/navigater.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+
+// Screens
+import homeScreen from './screens/homeScreen';
+import notificationScreen from './screens/notificationScreen';
+import profileScreen from './screens/profileScreen';
+
+// Auth Screens
+import signupScreen from './screens/auth/signupScreen';
+
+
+const RootTabs = TabNavigator({
+    home: {
+        screen: homeScreen,
+        navigationOptions: {
+            headerTitle: 'Home',
+        },
+    },
+    notifications: {
+        screen: notificationScreen,
+        navigationOptions: {
+            headerTitle: 'Home',
+        },
+    },
+    profile: {
+        screen: profileScreen,
+        navigationOptions: {
+            headerTitle: 'Home',
+        },
+    } ,
+});
+
+const RootNavigator = StackNavigator({
+    app: {
+        screen: RootTabs
+    }
+});
+
+
+const AuthNavigator = StackNavigator({
+    signup: {
+        screen: signupScreen
+    }
+});
+
+
+// export default RootNavigator;
+
+class AppNavigation extends Component {
+    state = {
+        isloggedin: false
+    }
+    render() {
+        if(!this.state.isloggedin) {
+            return <AuthNavigator />
+        }
+        return (
+           <RootNavigator /> 
+        );
+    }
+}
+
+export default AppNavigation;
